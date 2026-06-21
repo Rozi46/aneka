14592459d742009b90a03b377f0b09d8cc46cf08
systemR <roziyunaldo55@gmail.com>
2026-06-21 21:18:53 +0700

    first commit

--- system.bat
+++ system.bat
@@ -1,29 +1,29 @@
 @ECHO OFF
 
-cd /d D:\@systemR\local\eperdanamotor
+cd /d D:\@systemR\local\aneka
 
 
-start "Laravel 8028" cmd /k php artisan serve --host=127.0.0.1 --port=8028
-start "Laravel 8029" cmd /k php artisan serve --host=127.0.0.1 --port=8029
+start "Laravel 8030" cmd /k php artisan serve --host=127.0.0.1 --port=8030
+start "Laravel 8031" cmd /k php artisan serve --host=127.0.0.1 --port=8031
 
 echo Waiting for servers...
 
-:wait_8028
+:wait_8030
 timeout /t 2 >nul
-curl -s http://127.0.0.1:8028 >nul
-if errorlevel 1 goto wait_8028
+curl -s http://127.0.0.1:8030 >nul
+if errorlevel 1 goto wait_8030
 
-echo Server 8028 ready!
+echo Server 8030 ready!
 
-:wait_8029
+:wait_8031
 timeout /t 2 >nul
-curl -s http://127.0.0.1:8029 >nul
-if errorlevel 1 goto wait_8029
+curl -s http://127.0.0.1:8031 >nul
+if errorlevel 1 goto wait_8031
 
-echo Server 8029 ready!
+echo Server 8031 ready!
 
 echo All servers ready!
 
-start "" "C:\Program Files\Mozilla Firefox\firefox.exe" http://127.0.0.1:8028/admin/administration
+start "" "C:\Program Files\Mozilla Firefox\firefox.exe" http://127.0.0.1:8030/admin/administration
 
 exit
